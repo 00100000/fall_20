9b1702487f7e0ced77ac1df249233e328ba63938
00100000 <d10531318@gapps.fg.tp.edu.tw>
2020-12-18 02:08:21 +0800

    i am hungry

--- Algorithm/hw/fin_rewiew/1129.cpp
+++ Algorithm/hw/fin_rewiew/1129.cpp
@@ -1,15 +1,18 @@
 #include <cstdio>
 #include <stack>
+#include <queue>
 using namespace std;
 
 int n, m, q;
 int adja[5005][5005];
 int op[500005][3]; // i, j, success
+int pre[5005][2];
+int visit[5005];
 
 void swap(int &a, int &b){int t = a; a = b; b = t;}
 void bfs();
 void update(int u, int v);
-void query(int id);
+int query(int id);
 
 int main(){
     scanf("%d%d%d", &n, &m, &q);
@@ -31,5 +34,35 @@ int main(){
         op[i][1] = v;
     }
     bfs();
-    for(int i = q-1; i >= 0; i--)
+    stack<int> s;
+    for(int i = q-1; i >= 0; i--){
+        if(op[i][0] < 0) s.push( query(op[i][1]) );
+        else if(op[i][2]) update(op[i][0], op[i][1]);
+    }
+    while(!s.empty()){
+        printf("%d\n", s.top());
+        s.pop();
+    }
+}
+
+void bfs(){
+    for(int i = 0; i < n; i++){
+        if(!visit[i]){
+            int cnt = 1;
+        }
+    }
+}
+
+void update(int u, int v){
+    while(pre[u][0] != u) u = pre[u][0];
+    while(pre[v][0] != v) v = pre[v][0];
+    if(u != v){
+        pre[u][1] += pre[v][1];
+        pre[v][0] = u;
+    }
+}
+
+int query(int id){
+    while(pre[id][0] != id) id = pre[id][0];
+    return pre[id][1];
 }
